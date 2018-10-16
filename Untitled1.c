4a56ba6f346733b9c8658db1e5f420d86a227309
Abdul Monim <abdul.monim752@gmail.com>
2018-10-17 00:02:55 +0600

    first commit

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int wm;
+    cin>>wm;
+    if(wm%2==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
---
+++ Watermelon.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w;
+    while(scanf("%d",&w)==1)
+    {
+        if(w%2==0 && w!=2)
+        {
+           cout<<"YES"<<endl;
+        }
+       else
+       {
+           cout<<"NO"<<endl;
+       }
+    }
+    return 0;
+}
