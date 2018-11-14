d854bf563b581fb4fe4283ea7605bb73e40320d3
Abdul Monim <43042797+Monim752@users.noreply.github.com>
2018-11-14 10:47:28 +0600

    Add files via upload

---
+++ String_Task.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char str[100];
+    cin>>str;
+    for(int i=0; i<strlen(str); i++)
+    {
+        if(str[i]>=65 && str[i]<=90)
+        {
+            str[i]=97+str[i]-65;
+        }
+        if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u' && str[i]!='y')
+        {
+            cout<<"."<<str[i];
+        }
+    }
+    return 0;
+}
