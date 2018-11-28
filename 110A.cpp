c0ec01bbb43f8ca4fbcd7f1362b469bb9d35d2ec
Abdul Monim <43042797+Monim752@users.noreply.github.com>
2018-11-28 21:50:38 +0600

    Add files via upload

---
+++ 110A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace  std;
+int main()
+{
+    long long n;
+    int cnt=0;
+    cin>>n;
+    while(n!=0)
+    {
+        if(n%10==4 || n%10==7)
+        {
+            cnt+=1;
+        }
+        n=n/10;
+    }
+    if(cnt==4 || cnt==7)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
